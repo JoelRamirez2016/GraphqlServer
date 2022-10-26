5667ddad7f329a240e3dbbc3e5b6d704df6ae6bd
JoelRamirez2016 <joel.ramirez@correounivalle.edu.co>
2022-10-26 15:57:46 -0500

    fixing dependencies

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,6 @@
-// npm install @apollo/server graphql
 import { ApolloServer } from '@apollo/server';
 import { startStandaloneServer } from '@apollo/server/standalone';
+import config from '../config/config';
 import { context } from './app';
 import schema from './schemas';
 
@@ -8,8 +8,7 @@ const server = new ApolloServer({
   schema  
 });  
 
-
 startStandaloneServer(server, {
-  listen: { port: 4000 },
+  listen: { port: config.PORT },
   context
 }).then(({url}) => console.log(`🚀 Server ready at: ${url}`));    
